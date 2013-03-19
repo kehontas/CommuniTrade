b3975cc722eebde6b8a4bd801c85d9744db3b874
jessica <jgon91@gmail.com>
2013-03-18 23:24:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommuniTrade::Application.config.secret_token = '5ef30e56de13aaa259c8eeabec7294020e74e281b1901ccb9f60e25392866c922fd6bdd4deb9f1e8cdb91e188a127901c7e0880fb5b27297c3f8b177944a31aa'
